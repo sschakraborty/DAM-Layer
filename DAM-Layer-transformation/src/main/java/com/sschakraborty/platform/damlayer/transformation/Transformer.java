c3ddb8508e65a635373fc5081fae4d572a5fecab
sschakraborty <sschakraborty@hotmail.com>
2020-06-18 17:31:37 +0530

    Created DAM-Layer-transformation module for transforming beans.

---
+++ DAM-Layer-transformation/src/main/java/com/sschakraborty/platform/damlayer/transformation/Transformer.java
@@ -0,0 +1,4 @@
+package com.sschakraborty.platform.damlayer.transformation;
+
+public interface Transformer {
+}
---
+++ DAM-Layer-transformation/src/main/java/com/sschakraborty/platform/damlayer/transformation/TransformerImpl.java
@@ -0,0 +1,4 @@
+package com.sschakraborty.platform.damlayer.transformation;
+
+public class TransformerImpl implements Transformer {
+}
